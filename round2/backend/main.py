fe8c8f6923c66b722490a1d484bb662cae8f678c
FangDat <skyfall20192k4@gmail.com>
2026-05-22 11:33:35 +0700

    Update

--- round2/backend/main.py
+++ round2/backend/main.py
@@ -38,7 +38,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                   "https://intern-ten-gamma.vercel.app/"],
+                   "https://intern-ten-gamma.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

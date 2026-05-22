74535d31ad06cbfe2f42d1a7ef72a1590fd7ea88
FangDat <skyfall20192k4@gmail.com>
2026-05-22 11:43:26 +0700

    Update

--- round2/frontend/frontend/src/api.js
+++ round2/frontend/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://intern-production-7fe7.up.railway.app",
 });
 
 export default api;
